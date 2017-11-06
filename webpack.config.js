3359117c8f3fb3afbe3815ecc99d69310c0f631a
Nicholas Hoyt <nhoyt@illinois.edu>
2017-11-06 17:36:27 -0600

    Rename dist subdirectory to build

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     lists:       "./lists.js"
   },
   output: {
-    path: path.join(__dirname, "dist"),
+    path: path.join(__dirname, "build"),
     filename: "[name].js"
   },
   module: {
